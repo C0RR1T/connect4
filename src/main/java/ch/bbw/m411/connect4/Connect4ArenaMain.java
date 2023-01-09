e3dbe576f64f0e5f3d78f336d5bf3f5a928ad1f5
Corsin <corsin@ragett.li>
2023-01-09 10:22:20 +0100

    old algorithm

--- src/main/java/ch/bbw/m411/connect4/Connect4ArenaMain.java
+++ src/main/java/ch/bbw/m411/connect4/Connect4ArenaMain.java
@@ -17,7 +17,7 @@ public class Connect4ArenaMain {
     static final int NOMOVE = -1;
 
     public static void main(String[] args) {
-        new Connect4ArenaMain().play(new HumanPlayer(),new PerfectPlayer(10));
+        new Connect4ArenaMain().play(new PerfectPlayer(15), new HumanPlayer());
     }
 
     static String toDebugString(Stone[] board) {
--- src/main/java/ch/bbw/m411/connect4/PerfectPlayer.java
+++ src/main/java/ch/bbw/m411/connect4/PerfectPlayer.java
@@ -1,9 +1,7 @@
 package ch.bbw.m411.connect4;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 public class PerfectPlayer extends Connect4ArenaMain.DefaultPlayer {
     private Integer bestPlay = null;
@@ -42,14 +40,21 @@ public class PerfectPlayer extends Connect4ArenaMain.DefaultPlayer {
     }
 
     private int countEmpty() {
-        return (int) Arrays.stream(this.board)
-                .filter(Objects::isNull)
-                .count();
+        int count = 0;
+        for (Connect4ArenaMain.Stone stone : board) {
+            if (stone == null)
+                count++;
+        }
+        return count;
+    }
+
+    private int countFilled() {
+        return board.length - countEmpty();
     }
 
     private List<Integer> generateMoves() {
         var list = new ArrayList<Integer>();
-        for (int i = 0; i < 7; i++) {
+        for (Integer i : List.of(3, 2, 4, 5, 1, 0, 6)) {
             for (int y = i; y < board.length; y += 7) {
                 if (board[y] == null) {
                     list.add(y);
@@ -60,98 +65,47 @@ public class PerfectPlayer extends Connect4ArenaMain.DefaultPlayer {
         return list;
     }
 
+
     private int rate(Connect4ArenaMain.Stone forStone) {
         if (Connect4ArenaMain.isWinning(board, forStone))
-            return Integer.MAX_VALUE - 1;
-        else if (Connect4ArenaMain.isWinning(board, forStone.opponent()))
-            return Integer.MIN_VALUE + 1;
+            return Integer.MAX_VALUE;
+
+        if (Connect4ArenaMain.isWinning(board, forStone.opponent()))
+            return Integer.MIN_VALUE;
 
         var rating = 0;
-        var myHorizontal = 0;
-        var opponentHorizontal = 0;
-        var emptyFields = countEmpty();
-        for (int i = board.length - 1; i > 0; i--) {
-            // Horizontal reset
-            if (i == 20 || i == 13 || i == 6) {
-                myHorizontal = 0;
-                opponentHorizontal = 0;
-                rating -= ratingOfRow(opponentHorizontal);
-                rating += ratingOfRow(myHorizontal);
-            }
-
-            var currentStone = board[i];
-
-            if (currentStone == forStone) {
-                myHorizontal++;
-                rating -= ratingOfRow(opponentHorizontal);
-                opponentHorizontal = 0;
-            } else if (currentStone == forStone.opponent()) {
-                opponentHorizontal++;
-                rating += ratingOfRow(myHorizontal);
-                myHorizontal = 0;
-            }
-
-
+        for (int i = 0; i < board.length; i++) {
+            // Rate the move based on how close it is to the center
+            // i % 7 always goes to 6, then back to 0
             switch (i % 7) {
-                case 0, 6 -> rating += calcRating(currentStone, forStone, 1,
-                        emptyFields);
-                case 1, 5 -> rating += calcRating(currentStone, forStone, 2,
-                        emptyFields);
-                case 2, 4 -> rating += calcRating(currentStone, forStone, 3,
-                        emptyFields);
-                case 3 -> rating +=
-                        calcRating(currentStone, forStone, 4, emptyFields);
-            }
-
-            if (i > 20) {
-                rating += rowCounting((cur) -> cur - 7, i, forStone);
-
-                if (i <= 24) {
-                    rating += rowCounting((cur) -> cur - 7 + 1, i, forStone);
-                } else {
-                    rating += rowCounting((cur) -> cur - 7 - 1, i, forStone);
-                }
+                case 0, 6 -> rating += 1;
+                case 1, 5 -> rating += 2;
+                case 2, 4 -> rating += 3;
+                case 3 -> rating += 4;
             }
         }
         return rating;
     }
 
     private int ratingOfRow(int amount) {
-        return (int) Math.pow(amount, 2);
-    }
-
-    private int calcRating(Connect4ArenaMain.Stone stone,
-                           Connect4ArenaMain.Stone forColor, int amount,
-                           int empty) {
-        var newAmount = (int) ((1.0 / (empty / 20.0)) * 5.0);
-        if (stone == forColor)
-            return newAmount;
-        else if (stone == forColor.opponent())
-            return -amount;
-        else return 0;
+        return amount;
     }
 
     private int rowCounting(Step step, int origin,
                             Connect4ArenaMain.Stone forColor) {
         int rating = 0;
         var myCount = 0;
-        var opponentCount = 0;
         for (int i = origin; i >= 0; i = step.nextStep(i)) {
             var currentStone = board[i];
 
             if (currentStone == forColor) {
                 myCount++;
-                rating -= ratingOfRow(opponentCount);
-                opponentCount = 0;
             } else if (currentStone == forColor.opponent()) {
-                opponentCount++;
                 rating += ratingOfRow(myCount);
                 myCount = 0;
             } else {
                 rating += ratingOfRow(myCount);
                 myCount = 0;
-                rating -= ratingOfRow(opponentCount);
-                opponentCount = 0;
             }
         }
         return rating;
@@ -165,6 +119,7 @@ public class PerfectPlayer extends Connect4ArenaMain.DefaultPlayer {
     @Override
     int play() {
         bestPlay = null;
+        int originFilled = countFilled();
         minMax(Integer.MAX_VALUE, Integer.MIN_VALUE, this.maxDepth, myColor);
         return bestPlay;
     }
